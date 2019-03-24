7e7e29d3ec83548360b6dc9bd2b5006d1b37baa0
Facundo Cerezo <19938617+FacundoCerezo@users.noreply.github.com>
2019-03-24 20:08:51 -0300

    Added some comments to polls/models.py to aid in my own learning

--- polls/models.py
+++ polls/models.py
@@ -3,12 +3,12 @@ from django.db import models
 # Create your models here.
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    question_text = models.CharField(max_length=200) # question_text will be a column in the DB
+    pub_date = models.DateTimeField('date published') # 'date published' is set as a human-readable name
 
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE) # sets a relationship with a Question
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
